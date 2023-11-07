5b8dcbcc1ba54167fac6a37311ab09a6c3cca8dd
Antonio <Tonialon17@gmail.com>
2023-11-07 10:30:58 +0000

    Login Actualizado

---
+++ backend/config.js
@@ -0,0 +1,13 @@
+//Credenciales de la base de datos
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "1234",
+        database: "bdgestion",
+        port: 3307,
+        connectTimeout: 60000
+    },
+}
+
+module.exports = config;
